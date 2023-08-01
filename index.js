623bc6731f92ee7ed156655d6bf7f3b263a17cd2
Danny <danny.lucas@live.nl>
2023-08-02 01:15:34 +0200

    added part 1 of first challenge

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 let activeButtonID = "";
 
 const ACTIVE_BUTTON = 'active-tab-button';
-const setSelectedButtonStyling = (next) => {
-  const currentActiveButton = activeButtonID && document.getElementById(activeButtonID);
-  const nextActiveButton = document.getElementById(next);
+const ACTIVE_PANEL = 'active-panel';
 
-  if (currentActiveButton && currentActiveButton.classList.contains(ACTIVE_BUTTON)) {
-    currentActiveButton.classList.remove(ACTIVE_BUTTON);
+const switchActiveElement = (currentID, nextID, className) => {
+  const currentElement = currentID && document.getElementById(currentID);
+  const nextElement = document.getElementById(nextID);
+
+  if (currentElement && currentElement.classList.contains(className)) {
+    currentElement.classList.remove(className);
   }
 
-  if (!nextActiveButton.classList.contains(ACTIVE_BUTTON)) {
-    nextActiveButton.classList.add(ACTIVE_BUTTON);
+  if (!nextElement.classList.contains(className)) {
+    nextElement.classList.add(className);
   }
+} 
+
+const setSelectedButton = (currentID, nextID) => switchActiveElement(currentID, nextID, ACTIVE_BUTTON)
+const setActivePanel = (currentID, nextID) => switchActiveElement(currentID, nextID, ACTIVE_PANEL);
+
+const buttonToPanelMap = {
+  button_1: 'panel_1',
+  button_2: 'panel_2',
+  button_3: 'panel_3',
+  button_4: 'panel_4',
+  button_5: 'panel_5',
 }
 
-const onButtonClick = (id) => {
-  setSelectedButtonStyling(id);
-  activeButtonID = id;
+const onButtonClick = (nextID) => {
+  setSelectedButton(activeButtonID, nextID);
+  setActivePanel(buttonToPanelMap[activeButtonID], buttonToPanelMap[nextID]);
+  activeButtonID = nextID;
 }
